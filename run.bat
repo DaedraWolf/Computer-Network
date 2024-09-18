993b66e972fb77798922d61b13ca5c6c5f1a6095
anam <aaronnam7901@gmail.com>
2024-09-18 15:06:42 -0700

    replaced .bat with .ps1

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-docker run -v ${pwd}:/home/cse160 -ti ucmercedandeslab/tinyos_debian
-:: docker run --rm -v /:/app -w /app ucmercedandeslab/tinyos_debian:latest sh -c "make micaz sim"   $:: don't use this line
---
+++ run.ps1
@@ -0,0 +1,1 @@
+docker run -v ${pwd}:/home/cse160 -ti ucmercedandeslab/tinyos_debian
