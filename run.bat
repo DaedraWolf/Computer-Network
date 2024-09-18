f9e3290566de1367e2aa76e594e528c7837b14bb
anam <aaronnam7901@gmail.com>
2024-09-18 14:45:31 -0700

    added alternate command

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 @echo off
 docker run -v /:/home/cse160 -ti ucmercedandeslab/tinyos_debian
+:: docker run --rm -v /:/app -w /app ucmercedandeslab/tinyos_debian:latest sh -c "make micaz sim"   $:: don't use this line
